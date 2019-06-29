2b2e45c919fdf01fed21168ce69d1dc64517c927
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-28 17:19:06 -0700

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,3 @@
-# Write a method in the ChattyStudent class, #hello, that uses the super keyword to inherit the behavior of the #hello method from the parent, Student, class.
-#Then, augment that method to also puts out the very chatty phrase:
-# Write a method in the ChattyStudent class, raise_hand, that uses the super keyword ten times so that the method will puts out "Pick me!" ten times. It is possible to simply call super multiple times in a method.
-
-
 class ChattyStudent < Student
     def hello
         super
@@ -10,12 +5,6 @@ class ChattyStudent < Student
     end
 
     def raise_hand
-
         10.times { print super }
-
-        # 10.times do
-        #     puts "Pick me!"
-        # end
-
     end
 end
